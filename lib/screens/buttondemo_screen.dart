a84ef50ca4ac9910e633d82b2e355784aab79f1a
Nurul <nurulakter8@gmail.com>
2020-08-24 17:42:47 -0500

    rasied button commit

--- lib/screens/buttondemo_screen.dart
+++ lib/screens/buttondemo_screen.dart
@@ -14,14 +14,23 @@ class ButtonDemoScreen extends StatelessWidget {
           onPressed: () {
             print('rasied button1 is pressed');
           },
+          elevation: 10.0,
           child: Text('Raised button 1'),
           color: Colors.blue[900],
           textColor: Colors.white,
           splashColor: Colors.blue[100],
-          padding: EdgeInsets.only(// means how much space you want to put in the box
+          padding: EdgeInsets.only(
+              // means how much space you want to put in the box
               top: 5.0,
-              bottom: 10.0), 
+              bottom: 10.0),
         ),
+        RaisedButton.icon(
+            onPressed: () => print('raised button 2 is pressed'),
+            icon: Icon(
+              Icons.book,
+              color: Colors.red,
+            ),
+            label: Text('Raised Button 2'))
       ]),
     );
   }
